7afe91ae8bfe254e7b036e5fb3af6e6126a2de8a
Thiago <thiagodistend@hotmail.com>
2022-08-30 22:02:42 -0300

    Dockerfile

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -16,19 +16,20 @@ export default class Main extends Component{
     loadProducts = async() =>{
         const response = await api.get('/products');
 
-        this.setState({products : response.data.docs});
+        this.setState({products : response.data});
 
-        console.log(response.data.docs);
+        console.log(response.data);
     };
     render(){
         // return (<h1>Contagem de Produtos: {this.state.products.length}</h1>);
+        
         return(
             <div className="product-list">
                 {this.state.products.map(product =>(<article key={product._id}>
                 <strong>{product.title}</strong>
                 <p>{product.description}</p>
 
-                <a href="">Acessar</a>
+                <a href={product.url}>Acessar</a>
                 </article>))}
 
             </div>
